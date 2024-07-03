f4bc27ad28685fc2cce8ac1a0ca039013a4507a7
SMRFT <110091550+SMRFT@users.noreply.github.com>
2024-07-03 12:10:59 +0530

    Update settings.py

--- chatbot_backend/chatbot_backend/settings.py
+++ chatbot_backend/chatbot_backend/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',  # Add your frontend URL here
+    'http://localhost:3000', 
+    '15.207.192.151'# Add your frontend URL here
 ]
 
 # Optional: Allow credentials (cookies, authorization headers, etc.) to be included in cross-origin requests
